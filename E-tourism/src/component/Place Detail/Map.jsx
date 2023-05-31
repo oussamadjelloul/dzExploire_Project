c83af495f2858a5cb1a4ee1783bdbb229f2f6fc7
SifEddine05 <sifousamir72@gmail.com>
2023-05-31 13:25:09 +0100

    fix some problems

--- E-tourism/src/component/Place Detail/Map.jsx
+++ E-tourism/src/component/Place Detail/Map.jsx
@@ -2,7 +2,7 @@ import marker from '../../assets/Place Detail/Marker.png'
 import "leaflet/dist/leaflet.css"
 import { MapContainer, Marker, Popup, TileLayer} from 'react-leaflet';
 import { Icon } from 'leaflet';
-
+import '../../assets/css/place.css'
 
 const Map = () => {
     const customIcon = new Icon({
@@ -13,7 +13,7 @@ const Map = () => {
     
     <div className='w-full  shadow-md mt-6 p-4 bg-white '>
     <h3 className='text-[28px]  font-bold'>Card</h3>
-    <MapContainer center={[51.505, -0.09]} zoom={15} className='my-4' >
+    <MapContainer center={[51.505, -0.09]} zoom={15} className='my-4 ' >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
         <Marker position={[51.505,-0.09]} icon={customIcon}>
             <Popup>
