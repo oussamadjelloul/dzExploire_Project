8a01697635f5e060ab9716138f1777db4ed0dcbd
SifEddine05 <sifousamir72@gmail.com>
2023-06-14 03:21:33 +0100

    add the authentification and integrate add user page

---
+++ Admin/src/hooks/useLogout.jsx
@@ -0,0 +1,13 @@
+import { useAuthContext } from "./useAuthContext"
+
+
+
+export const useLogout =()=>{
+	const {dispatch} =  useAuthContext()
+	const logout =()=>{
+        dispatch({type : 'LOGOUT'})
+		localStorage.removeItem('user')
+	}
+	return {logout}
+
+}
--- Admin/src/main.jsx
+++ Admin/src/main.jsx
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AuthContextProvider } from './context/Auth.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter >
+    <AuthContextProvider >
       <App />
+      <ToastContainer />
+    </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
