b1774bc3c95b5136d541fc7932cc90b092bcb593
OUSSAMA DJELLLOUL <o.djelloul@esi-sba.dz>
2023-06-18 02:27:27 +0100

    fin

--- Admin/src/component/Dashboard/Nav.jsx
+++ Admin/src/component/Dashboard/Nav.jsx
@@ -1,25 +1,48 @@
+import React from "react";
 import { FaBars } from "react-icons/fa";
-import user from "../../assets/user.png"
+
+import userImage from "../../assets/user.png";
+import { getMe } from "../../api/api";
+import axios from "axios";
+import { useAuthContext } from '../../hooks/useAuthContext'
 
 const Nav = (prop) => {
-    return (  <div className="flex w-full lg:w-full lg:justify-end gap-2 pt-2 items-center justify-between p-1 fixed z-40 backdrop-filter backdrop-blur-md backdrop-brightness-100 pb-3">
-    <div className="w-1/12 lg:hidden">
-      <button
-        onClick={() => {
-          prop.setOpen(!prop.open);
-        }}
-      >
-        <FaBars size="25px" />
-      </button>
-    </div>
-    <div className="h-full flex justify-center items-center gap-[5%] ">
-      <img src={user} alt="admin" className="w-10 h-10"/>
-      <div className=" flex flex-col">
-        <p>OUSSAMA DJELLOUL</p>
-        <p>admin</p>
+  const { user} = useAuthContext();
+  const [users, setUser] = React.useState("");
+
+  React.useEffect(() => {
+    const fet = async () => {
+      const response = await fetch(getMe, {
+        method: 'GET',
+        headers: {
+          "Content-Type": "application/json",
+          'accestoken': user.token,
+        }
+      })
+      const jsonData = await response.json();
+      setUser(jsonData.user_name)
+    }
+    fet();
+  }, [])
+  return (
+    <div className="flex w-full lg:w-full lg:justify-end gap-2 pt-2 items-center justify-between p-1 fixed z-40 backdrop-filter backdrop-blur-md backdrop-brightness-100 pb-3">
+      <div className="w-1/12 lg:hidden">
+        <button
+          onClick={() => {
+            prop.setOpen(!prop.open);
+          }}
+        >
+          <FaBars size="25px" />
+        </button>
       </div>
-    </div>
-  </div> );
+      <div className="h-full flex w-[50%] md:w-[30%] lg:w-[20%] flex-end gap-[5%] ">
+        <img src={userImage} alt="admin" className="w-10 h-10" />
+        <div className=" flex flex-col">
+          <p>{users}</p>
+          <p>admin</p>
+        </div>
+      </div>
+    </div>);
 }
- 
+
 export default Nav;
